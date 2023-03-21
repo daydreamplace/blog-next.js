b86ae50e4f7e4107bd47e75c4a49bac8a56ceec1
youbin <you8inpark@gmail.com>
2023-03-21 17:12:44 +0900

    git init

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+const index = () => {
+  return <div></div>;
+};
+
+export default index;
---
+++ src/pages/post.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+const post = () => {
+  return <div></div>;
+};
+
+export default post;
