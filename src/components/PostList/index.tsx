a5b7eb874509d66eb5d8994d1cef545eeaee47a3
youbin <you8inpark@gmail.com>
2023-03-21 22:55:46 +0900

    feature: text overflow ellipsis

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,16 +1,56 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from '@emotion/styled';
-import { useTheme } from '@emotion/react';
 import Post from './post';
-import { theme } from '@/styles/theme';
+
+interface PostType {
+  title: string;
+  content: string;
+  created_at: string;
+}
+
+const posts = [
+  {
+    title: '111111111111111111111',
+    content:
+      '수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다. 함수를 미리 만들어두고 원할 때 실행해 정해진 동작을 수행할 수 있게 해준다. 수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다. 함수를 미리 만들어두고 원할 때 실행해 정해진 동작을 수행할 수 있게 해준다.',
+    created_at: '2014-03-17T14:03:17.000Z',
+  },
+  {
+    title: '2222222222222222222222222222222222222222222222222222222222',
+    content:
+      '수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다. 함수를 미리 만들어두고 원할 때 실행해 정해진 동작을 수행할 수 있게 해준다. 수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다. 함수를 미리 만들어두고 원할 때 실행해 정해진 동작을 수행할 수 있게 해준다.',
+    created_at: '2020-03-17T14:03:17.000Z',
+  },
+  {
+    title: '333333333333333333333333333333333333333333333333333333333333',
+    content:
+      '수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다. 함수를 미리 만들어두고 원할 때 실행해 정해진 동작을 수행할 수 있게 해준다. 수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다. 함수를 미리 만들어두고 원할 때 실행해 정해진 동작을 수행할 수 있게 해준다.',
+    created_at: '2020-03-17T14:03:17.000Z',
+  },
+];
 
 const PostList = () => {
+  const [postList, setPostList] = useState<PostType[]>([]);
+
+  useEffect(() => {
+    const loader = async () => {
+      try {
+        // await axios.get();
+        setPostList(posts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loader();
+  }, []);
   return (
     <Container>
       <h3>게시글</h3>
       <Border />
-      <Post />
-      <Post />
-      <Post />
+      {postList.map((post, index) => (
+        <Post key={index} title={post.title} content={post.content} date={post.created_at} />
+      ))}
     </Container>
   );
 };
--- src/components/PostList/post.tsx
+++ src/components/PostList/post.tsx
@@ -1,16 +1,18 @@
 import styled from '@emotion/styled';
 
-const Post = () => {
+interface PostType {
+  title: string;
+  content: string;
+  date: string;
+}
+
+const Post = ({ title, content, date }: PostType) => {
   return (
     <Container>
-      <h2 className='title'>[TIL] React에서 lazy loading 구현하기</h2>
-      <p className='content'>
-        수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다. 함수를 미리 만들어두고 원할 때 실행해
-        정해진 동작을 수행할 수 있게 해준다. 수학 시간에 y = f(x) 같은 함수를 배운 적이 있다. 프로그래밍에도 함수라는 개념이 존재한다. 수학의 함수와 비슷하지만, 다른 점도 있다. 프로그래밍에서 함수(function)는 일정한 동작을 수행하는 코드를 의미한다.
-        함수를 미리 만들어두고 원할 때 실행해 정해진 동작을 수행할 수 있게 해준다.
-      </p>
+      <h2 className='title'>{title}</h2>
+      <p className='content'>{content}</p>
       <div className='info'>
-        <span className='date'>2023년 3월 6일</span>&nbsp; · &nbsp;<span>0개의 댓글</span>
+        <span className='date'>{date}</span>&nbsp; · &nbsp;<span>0개의 댓글</span>
       </div>
     </Container>
   );
@@ -25,6 +27,9 @@ const Container = styled.div`
     font-size: 1.5rem;
     color: #212529;
     word-break: keep-all;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 
   .content {
